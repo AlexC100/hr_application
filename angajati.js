7115159e8b6ad45f427adeb808b51febb67abbe4
Alexandru Ceausu <alexandru.ceausu87@gmail.com>
2021-02-21 13:30:02 +0200

    add ui variables

---
+++ angajati.js
@@ -0,0 +1,11 @@
+// UI Variables
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const jobYears = document.querySelector('#job-years');
+const salary = document.querySelector('#salary');
+const position = document.querySelector('#position');
+const team = document.querySelector('#team');
+const phone = document.querySelector('#phone');
+const email = document.querySelector('#email');
+const paysTax = document.querySelector('#pays-tax');
+const submitBtn = document.querySelector('#submit');
